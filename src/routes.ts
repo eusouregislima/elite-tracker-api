ab7bf49f7aba45b1f1d44509807af0e4ac6dac8c
Reginaldo Lima <108771153+eusouregislima@users.noreply.github.com>
2024-05-06 17:45:08 -0300

    create focus time

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 
 import packageJson from '../package.json';
+import { FocusTimerController } from './controllers/focus-time.controller';
 import { HabitsController } from './controllers/habits.controller';
 
 export const routes = Router();
 
 const habitsController = new HabitsController();
+const focusTimerController = new FocusTimerController();
 
 routes.get('/', (request, response) => {
   const { name, description, version } = packageJson;
@@ -17,3 +19,5 @@ routes.get('/habits', habitsController.index);
 routes.post('/habits', habitsController.store);
 routes.delete('/habits/:id', habitsController.remove);
 routes.patch('/habits/:id/toggle', habitsController.toggle);
+
+routes.post('/focus-time', focusTimerController.store);
